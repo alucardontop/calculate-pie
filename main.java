6c316b1e66277c78d7c140c3bf5c7f9dac6c0e2f
alucard <alucardontop@gmail.com>
2023-03-06 21:35:30 +0530

    Update main.java

--- main.java
+++ main.java
@@ -3,18 +3,8 @@ import java.text.DecimalFormat;
 
 /**
  *  Calculate Pi 
- *  @date 19 August 2014
- *  @author Nefari0uss
- * 
- * This program will request the approximate number of calculations to run in calculating π.
- * The final result will be displayed on the console. Assumption is that the user inputs an int. 
- * 
- *
- * Uses the Gottfried Leibniz formula for calculation of π:
- *
- * 1 -  1/3  + 1/5 - 1/7 + 1/9 - ... = π/4
- *  
- * Source: Wikipedia - Leibniz formula for π
+ *  @date March 6 2023
+ *  @author alucard
  **/
 
 public class Pi {
